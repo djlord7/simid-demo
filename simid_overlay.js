9a88b05461daa53795a5ccdcf1dccb95e55a22d0
djlord7 <dhwajgoyal7@gmail.com>
2025-06-20 11:56:24 +0530

    Update simid_overlay.js

--- simid_overlay.js
+++ simid_overlay.js
@@ -6,12 +6,18 @@ class SimidOverlay extends BaseSimidCreative {
   }
 
   showPlayableAd() {
-    const iframe = document.createElement('iframe');
-    iframe.src = 'https://tech-iion.github.io/advertiser-creatives/allKinds/';
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.border = 'none';
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://tech-iion.github.io/advertiser-creatives/allKinds/';
+  iframe.style.border = 'none';
+  iframe.style.display = 'block';
+  iframe.style.margin = '0 auto';
 
-    this.adContainer.appendChild(iframe);
+  // Dynamically size to fit the player's creative dimensions
+  const width = this.environmentData?.creativeDimensions?.width || '100%';
+  const height = this.environmentData?.creativeDimensions?.height || '100%';
+  iframe.style.width = typeof width === 'number' ? width + 'px' : width;
+  iframe.style.height = typeof height === 'number' ? height + 'px' : height;
+
+  this.adContainer.appendChild(iframe);
   }
 }
